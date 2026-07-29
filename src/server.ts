1d2c1c131db6208e2fae1fa16f6ed2dda343849d
FacuLedesmaBertalot <fanelebe.contacto@gmail.com>
2026-07-29 12:43:51 -0300

    DB Conectada

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import express from 'express';
 import colors from 'colors';
 import morgan from 'morgan';
+import { db } from './config/db.js';
+
+async function conectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log(colors.bgGreen.bold('Conexión exitosa a la DB'));
+    } catch (error) {
+        console.log(colors.red.bold('Falló la Conexión a la DB'));
+    }
+}
+
+conectDB();
 
 const app = express();
 
