29bfd501cff6fce68cf501c441550f52b817d748
FacuLedesmaBertalot <fanelebe.contacto@gmail.com>
2026-07-29 13:11:03 -0300

    Created Budget

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,19 @@
 import { Sequelize } from 'sequelize-typescript';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 dotenv.config();
 
-export const db = new Sequelize( process.env.DATABASE_URL, {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+export const db = new Sequelize(process.env.DATABASE_URL!, {
+    models: [__dirname + '/../models/**/*'],
+    logging: false,
     dialectOptions: {
         ssl: {
-            require:false
+            require: false
         }
     }
-})
+});
